6b49c94f68b3e92924594dc916383761e3cad334
Kristijonas-St <kristijonasstanelis@gmail.com>
2025-08-20 21:06:09 +0300

    binary_tree: search returns parent now, add deletion of leaf

--- binary_trees/binary_tree.c
+++ binary_trees/binary_tree.c
@@ -10,6 +10,8 @@ typedef struct node {
 void insert_node(node**, int);
 node* search_node(node**, int);
 void delete_node(node**, int);
+void delete_leaf(node**, int);
+
 void print_tree(node*, int);
 
 void insert_node(node** root, int value)
@@ -57,24 +59,61 @@ void insert_node(node** root, int value)
 
 node* search_node(node** root, int val)
 {
-    node* temp = *root;
+    node* parent = *root;
+    node* temp;
 
-    while(temp != NULL) {
+    if((*root)->val == val) {
+        printf("It's root, need special attention here\n");
+        return NULL;
+    }
+
+    temp = (val > parent->val) ? parent->right_child : parent->left_child;
+    while(temp != NULL) { // !temp doesnt work here????
         if(temp->val == val) {
-            return temp;
+            printf("Found %d\n", val);
+            return parent;
         } else {
-            temp = (val < temp->val) ? temp->left_child : temp->right_child;
+            parent = temp;
+            temp = (val > temp->val) ? temp->right_child : temp->left_child;
         }
     }
+
     return NULL;
 }
 
-void delete_node(node** root, int value)
+void delete_node(node** root, int val)
 {
+    node* parent = search_node(root, val);
+    if(parent == NULL) {
+        printf("Node %d doesn't exist, returning...\n", val);
+        return;
+    }
+
+    node* to_delete = (val > parent->val) ? parent->right_child : parent->left_child;
     
+    if(to_delete->left_child == NULL && to_delete->right_child == NULL) {
+        delete_leaf(&parent, val);
+    } else if(to_delete->left_child != NULL && to_delete->right_child != NULL) {
+        printf("Goind to delete a node with TWO children\n");
+    } else {
+        printf("Going to delete a node with ONE child\n");
+    }
 }
 
-
+void delete_leaf(node** temp, int val)
+{
+    printf("%d: Goind to delete a LEAF\n", val);
+    node* leaf_to_delete;
+    
+    if(val > (*temp)->val) {
+        leaf_to_delete = (*temp)->right_child;
+        (*temp)->right_child = NULL;
+    } else {
+        leaf_to_delete = (*temp)->left_child;
+        (*temp)->left_child = NULL;
+    }
+    free(leaf_to_delete);  
+}
 
 // not mine, just to check the functionality
 void print_tree(node* root, int space)
@@ -92,33 +131,27 @@ void print_tree(node* root, int space)
     print_tree(root->left_child, space);
 }
 
-
 int main(void)
 {
     node* root = NULL;
-    node* search_result;
+    node* search_result, *temp;
 
     insert_node(&root, 6);
     insert_node(&root, 4);
     insert_node(&root, 3);
     insert_node(&root, 5);
     insert_node(&root, 7);
-    insert_node(&root, 10);
-    insert_node(&root, 11);
     insert_node(&root, 8);
-    insert_node(&root, 1);
-    insert_node(&root, 2);
-    insert_node(&root, 9);
-    insert_node(&root, 20);
-
-    // search example
-    int value = 11;
-    search_result = search_node(&root, value);
-    if(search_result == NULL) {
-        printf("%d node doesn't exist!!!\n", value);
-    } else {
-        printf("FOUND %d\n", value);
-    }
 
     print_tree(root, 0);
+    printf("\n\n");
+
+    delete_node(&root, 8);
+    print_tree(root, 0);
+    printf("\n\n");
+
+    delete_node(&root, 7);
+    print_tree(root, 0);    
+    printf("\n\n");
+
 }
