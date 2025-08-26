9ee667c95cfc515061ac08713c4968de4b1db07e
Kristijonas-St <kristijonasstanelis@gmail.com>
2025-08-26 19:24:04 +0300

    binary_tree: add checks for existing nodes, extract various functionalities into different functions

--- binary_trees/binary_tree.c
+++ binary_trees/binary_tree.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 typedef struct node {
     struct node* left_child;
@@ -14,72 +15,65 @@ void delete_leaf(node**, int);
 void delete_one_child(node**, int);
 void delete_two_children(node **, int);
 void delete_root(node **, int);
+void find_and_replace_with_bigger_smallest(node **, int);
 void print_tree(node*, int);
+bool already_exists(int, node**);
+bool root_is_parent(node**, int);
 
-void insert_node(node** root, int value)
+void insert_node(node** root, int val)
 {
+    printf("Insert %d\n", val);
+        
+    // Tree is empty
     if(!*root) {
         *root = (node*)malloc(sizeof(node));
-        (*root)->val = value;
+        (*root)->val = val;
         (*root)->left_child = NULL;
         (*root)->right_child = NULL;
-    
+        return;
+    }
+
+    // Prevent duplication
+    if(already_exists(val, root)) {
+        printf("This node ALREADY exists, exiting...\n");
         return;
     }
 
     node* iteration = *root;
     node* new_node;
-
-    int i = 0;
     while(1) {
-        if(value < iteration->val) {
+        // Go left
+        if(val < iteration->val) {
 
             if(iteration->left_child == NULL) {
                 new_node = (node*)malloc(sizeof(node));
                 iteration->left_child = new_node;
-                new_node->val = value;
+                new_node->val = val;
                 return;
             } else {
                 iteration = iteration->left_child;
             }
 
-        } else {
+        } 
+        // Go right
+        else {
             if(iteration->right_child == NULL) {
                 new_node = (node*)malloc(sizeof(node));
                 iteration->right_child = new_node;
-                new_node->val = value;            
+                new_node->val = val;            
                 return;
             } else {
                 iteration = iteration->right_child;
             }
 
         }
-    // printf("i amount: %d\n", ++i);
     }
-    
 }
 
 node* search_node(node** root, int val)
 {   
-    // For checking if root is the parent BEGIN
-    node *l, *r;
-    if((*root)->left_child != NULL) {
-        l = (*root)->left_child;
-        if(l->val == val) {
-            return *root;
-        }
-    }
-
-    if((*root)->right_child != NULL) {
-        r = (*root)->right_child;
-        if(r->val == val) {
-            return *root;
-        }
-    }
-    // For checking if root is the parent END
-
-
-    if(val == (*root)->val) {
+    
+    if(root_is_parent(root, val)) {
         return *root;
     }
 
@@ -87,7 +81,7 @@ node* search_node(node** root, int val)
     node* temp;
 
     temp = (val > parent->val) ? parent->right_child : parent->left_child;    
-    while(temp != NULL) { // !temp doesnt work here????
+    while(temp) {
         if(temp->val == val) {
             return parent;
         } else {
@@ -104,19 +98,20 @@ node* search_node(node** root, int val)
 void delete_node(node** root, int val)
 {    
     printf("Delete %d\n", val);
-    // Need to delete the root;
-    if(val == (*root)->val) {
-        delete_root(root, val);
-        return;
-    } 
-    
-    node* parent = search_node(root, val);
-    if(parent == NULL) {
+
+    // Do not attempt to delete a node that doesn't exist
+    if(!already_exists(val, root) && val != (*root)->val) {
         printf("Node %d doesn't exist, returning...\n", val);
         return;
     } 
 
-    // If it exists, just proceed...
+    // Need to delete the root
+    if(val == (*root)->val) {
+        delete_root(root, val);
+        return;
+    }
+
+    node* parent = search_node(root, val);
     node* to_delete = (val > parent->val) ? parent->right_child : parent->left_child;
     
     if(to_delete->left_child == NULL && to_delete->right_child == NULL) {
@@ -140,7 +135,8 @@ void delete_one_child(node** temp, int val)
     } else {
         (*temp)->right_child = new_child;
     }
-    // free(to_delete);
+    free(to_delete);
+    to_delete = NULL;
 }
 
 void delete_two_children(node** temp, int val)
@@ -150,11 +146,10 @@ void delete_two_children(node** temp, int val)
     node* node_to_be_deleted = (val > (*temp)->val) ? (*temp)->right_child : (*temp)->left_child;
     node* bigger_smallest = node_to_be_deleted->right_child;
 
-    // eini vienakart i desine, tada kol gali i kaire 
+    // Find a node that's bigger, but is the smallest on the right subtree
     while(bigger_smallest->left_child != NULL) {
         bigger_smallest = bigger_smallest->left_child;
     }
-    printf("to_delete: %d\tbigger_smallest %d\n", node_to_be_deleted->val, bigger_smallest->val);
 
     // replace one another
     node_to_be_deleted->val = bigger_smallest->val;
@@ -197,14 +192,17 @@ void delete_leaf(node **parent, int val)
 
 void delete_root(node **root, int val)
 {
+    // Root has two children
     if((*root)->left_child != NULL && (*root)->right_child != NULL) {
-        goto quick_test;
-        
-    } else if((*root)->left_child == NULL && (*root)->right_child == NULL) {
+        find_and_replace_with_bigger_smallest(root, val);
+    } 
+    // Root doesn't have any children
+    else if((*root)->left_child == NULL && (*root)->right_child == NULL) {
         free(*root);
         *root = NULL;
-        return;
-    } else if((*root)->right_child == NULL && (*root)->left_child != NULL) {
+    } 
+    // Root doesn't have the left child;
+    else if((*root)->right_child == NULL && (*root)->left_child != NULL) {
         node* old_left_child = (*root)->left_child;
         node* new_left_child = old_left_child->left_child;
         node* old_root = *root;
@@ -214,55 +212,101 @@ void delete_root(node **root, int val)
 
         free(old_root);
         old_root = NULL;
-
     } 
+    // Root doesn't have the right child
     else {
-quick_test:        
-        
-        node* r = (*root)->right_child;
-        node* bigger_smallest;
-        
-
-        if(r->left_child == NULL) {
-            node* old_right_child = (*root)->right_child;
-            node* new_right_child = old_right_child->right_child;
-            node* old_left_child = (*root)->left_child;
-            node* old_root = *root;
-
-            *root = old_right_child;
-            (*root)->right_child = new_right_child;
-            (*root)->left_child = old_left_child;
-
-            free(old_root);
-            old_root = NULL;            
-            return;
-        } else {
-            bigger_smallest = (*root)->right_child;
-        }
-
-        while(bigger_smallest->left_child != NULL) {
-            bigger_smallest = bigger_smallest->left_child;
-        }
-
-        // replace one another
-        int temp_root = (*root)->val;
-        (*root)->val = bigger_smallest->val;
-        bigger_smallest->val = temp_root;
-
-        // delete old_root_val
-        node* right_subtree = (*root)->right_child;
-        if(right_subtree->val == val) {
-            delete_leaf(root, val);
-            return;    
-        }
-
-        node* to_delete = search_node(&right_subtree, val);
-        delete_leaf(&to_delete, val);
-        return;
+        find_and_replace_with_bigger_smallest(root, val);
     }
 }
 
-// not mine, just to check the functionality
+void find_and_replace_with_bigger_smallest(node** root, int val)
+{
+        
+    node* r = (*root)->right_child;
+    node* bigger_smallest;
+    
+    if(r->left_child == NULL) {
+        node* old_right_child = (*root)->right_child;
+        node* new_right_child = old_right_child->right_child;
+        node* old_left_child = (*root)->left_child;
+        node* old_root = *root;
+
+        *root = old_right_child;
+        (*root)->right_child = new_right_child;
+        (*root)->left_child = old_left_child;
+
+        free(old_root);
+        old_root = NULL;            
+        return;
+    } else {
+        bigger_smallest = (*root)->right_child;
+    }
+
+    while(bigger_smallest->left_child != NULL) {
+        bigger_smallest = bigger_smallest->left_child;
+    }
+
+    // Replace one another
+    int temp_root = (*root)->val;
+    (*root)->val = bigger_smallest->val;
+    bigger_smallest->val = temp_root;
+
+    // Delete old_root_val
+    node* right_subtree = (*root)->right_child;
+    if(right_subtree->val == val) {
+        delete_leaf(root, val);
+        return;    
+    }
+
+    node* to_delete = search_node(&right_subtree, val);
+    delete_leaf(&to_delete, val);
+    return;
+}
+
+bool already_exists(int val, node** root)
+{
+    // Prevent duplication
+    node* prevent_duplication = search_node(root, val);
+    if(prevent_duplication != NULL) {
+        node *l = prevent_duplication->left_child, *r = prevent_duplication->right_child;
+        if(val == (*root)->val) {
+            return true;
+        }
+        if(l != NULL) {
+            if(val == l->val) {
+                return true;
+            }
+        }
+        if(r != NULL) {
+            if(val == r->val) {
+                return true;
+            }
+        }
+    } 
+    return false;
+}
+
+bool root_is_parent(node** root, int val)
+{
+    node *l, *r;
+    if((*root)->left_child != NULL) {
+        l = (*root)->left_child;
+        if(l->val == val) {
+            return true;
+        }
+    }
+
+    if((*root)->right_child != NULL) {
+        r = (*root)->right_child;
+        if(r->val == val) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+// Not mine, just to visually see the tree
 void print_tree(node* root, int space)
 {
     if(root == NULL) return;
@@ -287,8 +331,6 @@ int main(void)
     insert_node(&root, 40);
     insert_node(&root, 60);
     
-
-
     printf("Initial tree\n");
     if(root != NULL) {
         print_tree(root, 0);
@@ -301,6 +343,7 @@ int main(void)
         printf("\n__________________________\n");    
     } else printf("Tree is empty\n");
 
+    
     insert_node(&root, 10);
     if(root != NULL) {
         print_tree(root, 0);
@@ -343,20 +386,19 @@ int main(void)
         printf("\n__________________________\n");    
     } else printf("Tree is empty\n");
 
-    // Delete root with two children
-    delete_node(&root, 40);
-    if(root != NULL) {
-        print_tree(root, 0);
-        printf("\n__________________________\n");    
-    } else printf("Tree is empty\n"); 
-
-    delete_node(&root, 45);
+    insert_node(&root, 80);
     if(root != NULL) {
         print_tree(root, 0);
         printf("\n__________________________\n");    
     } else printf("Tree is empty\n");
 
-    delete_node(&root, 50);
+    delete_node(&root, 55);
+    if(root != NULL) {
+        print_tree(root, 0);
+        printf("\n__________________________\n");    
+    } else printf("Tree is empty\n");
+
+    delete_node(&root, 40);
     if(root != NULL) {
         print_tree(root, 0);
         printf("\n__________________________\n");    
@@ -367,16 +409,5 @@ int main(void)
         print_tree(root, 0);
         printf("\n__________________________\n");    
     } else printf("Tree is empty\n");
-
-    delete_node(&root, 80);
-    if(root != NULL) {
-        print_tree(root, 0);
-        printf("\n__________________________\n");    
-    } else printf("Tree is empty\n");
-
-    insert_node(&root, 101);
-    if(root != NULL) {
-        print_tree(root, 0);
-        printf("\n__________________________\n");    
-    } else printf("Tree is empty\n");
 }
+
